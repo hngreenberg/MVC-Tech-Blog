82a94dec48cfa88cbd8ed3298493d94edaa1107e
Heather Greenberg <hgreenberg1017@gmail.com>
2023-03-28 12:43:43 -0700

    building routes

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const blogRoutes = require('./blog-routes');
+const commentRoutes = require('./comment-routes');
+
+router.use('/users', userRoutes);
+router.use('/blogs, blogRoutes');
+router.user('/comments', commentRoutes);
+
+module.exports = router;
